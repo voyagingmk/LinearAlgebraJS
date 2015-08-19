ade01dfff29f7267b9fd2af5d2e0a045b40f089b
voyagingmk@gmail.com <voyagingmk@gmail.com>
2015-08-20 00:55:12 +0800

    transpose power divide

--- src/matrix.js
+++ src/matrix.js
@@ -2,6 +2,7 @@ var Class = require("./class");
 
 var Matrix = Class.extend({
 	init: function(data, row, col) {
+		this.m_Type = 1; //elem type, 1: Int 2: Float
 		this.m_data = data;
 		this.m_row = row;
 		this.m_col = col;
@@ -48,12 +49,19 @@ var Matrix = Class.extend({
 			data1[i] -= data2[i];
 		}
 	},
+	divide: function() {
+		if (typeof m == "number") {
+			if (m != 0)
+				return this.multiply(1 / m);
+		}
+	},
 	multiply: function(m) {
 		if (typeof m == "number") {
 			var data = this.m_data;
 			for (var i = 0, len = this.m_Len; i < len; i++) {
 				data[i] *= m;
 			}
+			return this;
 		} else {
 			if (this.m_col != m.m_row) {
 				console.error("[Matrix.multiply] can't multiply.");
@@ -67,7 +75,7 @@ var Matrix = Class.extend({
 				for (var c = 0; c < result.m_col; c++) {
 					var total = 0;
 					for (var k = 0; k < K; k++) {
-						total += data1[r * this.m_col + k] * data1[k * m.m_col + c];
+						total += data1[r * this.m_col + k] * data2[k * m.m_col + c];
 					}
 					result.m_data[r * m.m_col + c] = total;
 				}
@@ -79,6 +87,37 @@ var Matrix = Class.extend({
 		if (!this.isSameOrder(m)) {
 			return false;
 		}
+		var data1 = this.m_data,
+			data2 = m.m_data;
+		for (var i = 0, len = this.m_Len; i < len; i++) {
+			if (data1[i] == data2[i])
+				continue;
+			if (this.m_Type == 2) {
+				if (Math.abs(data1[i] - data2[i]) < 1e-6)
+					continue;
+			}
+			return false;
+		}
+		return true;
+	},
+	transpose: function() {
+		var result = new Matrix([], this.m_col, this.m_row);
+		for (var r = 0; r < this.m_row; r++) {
+			for (var c = 0; c < this.m_col; c++) {
+				result.m_data[c * result.m_col + r] = this.m_data[r * this.m_col + c];
+			}
+		}
+		return result;
+	},
+	power: function(n) {
+		if (this.m_row != this.m_col)
+			return;
+		//TODO: below is simple algorithm
+		var result = this.clone();
+		for(var i = 0; i < n - 1; i++){
+			result = result.multiply(this);
+		}
+		return result;
 	},
 	debug: function(msg) {
 		msg = (msg && msg + "\n") || "";
@@ -91,5 +130,7 @@ var Matrix = Class.extend({
 		console.log(msg);
 	},
 });
+
+
 
 module.exports = Matrix;
--- src/test.js
+++ src/test.js
@@ -20,6 +20,61 @@ var m3 = m1.multiply(m2);
 
 m3.debug("m3:");
 
-var m4 = m2.add(m3);
+m33 = m1.multiply(m1);
+m33.debug("m33:");
+
+var m4 = m2.clone().add(m3);
 
 m4.debug("m4:");
+
+console.log("m3 and m4 is equal:", m3.isEqual(m4), "\n");
+
+
+var m5 = new Matrix([
+	1,4,
+	2,5,
+	3,6,
+], 3, 2);
+var m5t = m5.transpose();
+m5.debug("m5:");
+m5t.debug("m5t:");
+
+var m6 = new Matrix([
+	1,
+	2,
+	3,
+	4,
+], 4, 1);
+var m6t = m6.transpose();
+var m6tt = m6t.transpose();
+m6.debug("m6:");
+m6t.debug("m6t:");
+m6tt.debug("m6tt:");
+
+console.log("[k(M)T] and [(kM)T] is equal:\n", m6.clone().multiply(3).transpose().isEqual( m6.clone().transpose().multiply(3) ));
+
+var m7 = new Matrix([
+	1,2,
+	3,4,
+], 2, 2);
+var m8 = new Matrix([
+	-2,-1,
+	-4,-3,
+], 2, 2);
+console.log("[(AB)T] and [(B)T*(A)T] is equal:\n", m7.clone().multiply(m8.clone()).transpose().isEqual( m7.clone().transpose().multiply(m8.clone().transpose()) ));
+
+console.log("AB and BA is equal:\n",m7.clone().multiply(m8.clone()).isEqual(m8.clone().multiply(m7.clone())));
+
+m7 = new Matrix([
+	1,1,
+	0,1,
+], 2, 2);
+var m7power2 = m7.power(2);
+m7power2.debug("m7power2:");
+var m7_power2_mul_power2 = m7.power(2).multiply(m7.power(2));
+m7_power2_mul_power2.debug("m7_power2_mul_power2:");
+var m7power4 = m7.power(4);
+m7power4.debug("m7power4:");
+var m7power20 = m7.power(20);
+m7power20.debug("m7power20:");
+
